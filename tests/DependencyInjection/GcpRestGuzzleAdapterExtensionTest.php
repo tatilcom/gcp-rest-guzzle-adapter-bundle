d490f6a7ea530f339fd72f3ff8acecfca1a0525f
yali4 <yalcinceylan93@gmail.com>
2020-12-02 20:54:41 +0300

    compatible with symfony 4 and symfony 5.

--- tests/DependencyInjection/GcpRestGuzzleAdapterExtensionTest.php
+++ tests/DependencyInjection/GcpRestGuzzleAdapterExtensionTest.php
@@ -10,7 +10,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
 
-class GcpRestGuzzleAdapterExtensionTest extends \PHPUnit_Framework_TestCase
+class GcpRestGuzzleAdapterExtensionTest extends \PHPUnit\Framework\TestCase
 {
     public function testFooClient()
     {
